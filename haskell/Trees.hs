2cce3c2167a05ac50d6af2bd269a7c043825a945
Ilya Portnov <portnov@iportnov.ru>
2013-08-09 21:36:56 +0600

    Something is generated.

--- haskell/Trees.hs
+++ haskell/Trees.hs
@@ -49,7 +49,7 @@ instance Show Expression where
   show (Const True)  = "1"
   show (Var i) = "x" ++ show i
   show (If e1 e2 e3) = printf "(if %s %s %s)" (show e1) (show e2) (show e3)
-  show (Fold e1 e2 v1 v2 fn) = printf "(fold %s %s (lambda (%s %s) %s)" (show e1) (show e2) (show v1) (show v2) (show fn)
+  show (Fold e1 e2 v1 v2 fn) = printf "(fold %s %s (lambda (x%s x%s) %s)" (show e1) (show e2) (show v1) (show v2) (show fn)
   show (Op1 op e) = printf "(%s %s)" (show op) (show e)
   show (Op2 op e1 e2) = printf "(%s %s %s)" (show op) (show e1) (show e2)
 
@@ -81,7 +81,7 @@ instance Show Op2 where
   show Xor = "xor"
   show Plus = "plus"
 
-data AnyOp = A1 Op1 | A2 Op2 | AFold | AIf
+data AnyOp = A1 Op1 | A2 Op2 | AFold | ATFold | AIf
   deriving (Eq, Show)
 
 data GState = GState {
@@ -101,8 +101,13 @@ class Generated a where
 instance Generated Id where
   generate 1 _ = do
     n <- gets lastVariable
+    return [1..n]
+
+newVariable :: Generate Id
+newVariable = do
+    n <- gets lastVariable
     modify $ \st -> st {lastVariable = n+1}
-    return [n+1]
+    return (n+1)
 
 instance Generated Op1 where
   generate 1 list = return [op | A1 op <- list]
@@ -118,7 +123,8 @@ instance Generated Expression where
   generate size ops = do
     ifs <- if AIf `elem` ops
              then do
-                  let sizes = split 3 size
+                  let sizes = split 3 (size-1)
+                  lift $ putStrLn $ printf "If size=%d, sizes: %s" size (show sizes)
                   concatFor sizes $ \([sizeCond, sizeE1, sizeE2]) -> do
                     conds <- generate sizeCond ops
                     e1s   <- generate sizeE1 ops
@@ -128,7 +134,46 @@ instance Generated Expression where
     let ops_wo_fold = filter (/= AFold) ops
     folds <- if AFold `elem` ops
                then do
+                    let sizes = split 3 (size-2)
+                    lift $ putStrLn $ printf "Fold size=%d, sizes: %s" size (show sizes)
+                    concatFor sizes $ \([sizeE0, sizeE1, sizeE2]) -> do
+                      e0s <- generate sizeE0 ops_wo_fold
+                      e1s <- generate sizeE1 ops_wo_fold
+                      x <- newVariable
+                      y <- newVariable
+                      e2s <- generate sizeE2 ops_wo_fold
+                      modify $ \st -> st {lastVariable = lastVariable st - 2}
+                      return [Fold e0 e1 x y e2 | e0 <- e0s, e1 <- e1s, e2 <- e2s]
+               else return []
+    tfolds <- if ATFold `elem` ops
+               then do
+                    let sizes = split 3 (size-2)
+                    lift $ putStrLn $ printf "TFold size=%d, sizes: %s" size (show sizes)
+                    concatFor sizes $ \([sizeE0, sizeE1, sizeE2]) -> do
+                      e0s <- generate sizeE0 ops_wo_fold
+                      e1s <- generate sizeE1 ops_wo_fold
+                      x <- newVariable
+                      e2s <- generate sizeE2 ops_wo_fold
+                      modify $ \st -> st {lastVariable = lastVariable st - 1}
+                      return [Fold e0 e1 x 0 e2 | e0 <- e0s, e1 <- e1s, e2 <- e2s]
+               else return []
+    op1s <- case [op | A1 op <- ops] of
+              [] -> return []
+              o1s -> do
+                     es <- generate (size-1) ops
+                     return [Op1 op e | op <- o1s, e <- es]
+    op2s <- case [op | A2 op <- ops] of
+              [] -> return []
+              o2s -> do
+                     let sizes = split 2 (size-1)
+                     lift $ putStrLn $ printf "Op2 size=%d, sizes: %s" size (show sizes)
+                     concatFor sizes $ \([sizeE1, sizeE2]) -> do
+                       e1s <- generate sizeE1 ops
+                       e2s <- generate sizeE2 ops
+                       return [Op2 op e1 e2 | op <- o2s, e1 <- e1s, e2 <- e2s]
+
+
                     
-    return ifs
+    return $ op1s ++ op2s ++ folds ++ tfolds ++ ifs
                   
     
